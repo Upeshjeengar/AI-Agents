30c07e179a65a5dcb5b482a93ef2738d7be11b17
Upesh <upeshjeengar@gmail.com>
2025-11-06 11:08:52 +0530

    locally working Dummy agent created with ollama

---
+++ google-adk/my_agent/__init__.py
@@ -0,0 +1,78 @@
+import datetime
+from zoneinfo import ZoneInfo
+from google.adk.agents import Agent
+import os
+from google.adk.models.lite_llm import LiteLlm
+# from dotenv import load_dotenv
+# load_dotenv()
+# OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+# os.environ["OPENAI_API_BASE_URL"] = "http://localhost:11434/v1"
+def get_weather(city: str) -> dict:
+    """Retrieves the current weather report for a specified city.
+
+    Args:
+        city (str): The name of the city for which to retrieve the weather report.
+
+    Returns:
+        dict: status and result or error msg.
+    """
+    if city.lower() == "new york":
+        return {
+            "status": "success",
+            "report": (
+                "The weather in New York is sunny with a temperature of 25 degrees"
+                " Celsius (77 degrees Fahrenheit)."
+            ),
+        }
+    else:
+        return {
+            "status": "error",
+            "error_message": f"Weather information for '{city}' is not available.",
+        }
+
+
+def get_current_time(city: str) -> dict:
+    """Returns the current time in a specified city.
+
+    Args:
+        city (str): The name of the city for which to retrieve the current time.
+
+    Returns:
+        dict: status and result or error msg.
+    """
+
+    if city.lower() == "new york":
+        tz_identifier = "America/New_York"
+    else:
+        return {
+            "status": "error",
+            "error_message": (
+                f"Sorry, I don't have timezone information for {city}."
+            ),
+        }
+
+    tz = ZoneInfo(tz_identifier)
+    now = datetime.datetime.now(tz)
+    report = (
+        f'The current time in {city} is {now.strftime("%Y-%m-%d %H:%M:%S %Z%z")}'
+    )
+    return {"status": "success", "report": report}
+
+model = LiteLlm(
+    model = "openai/llama3.1",
+    api_key = "ollama",
+    base_url = "http://localhost:11434/v1"  # This is the missing piece
+)
+
+root_agent = Agent(
+    name="weather_time_agent",
+    model=model,
+    
+    description=(
+        "Agent to answer questions about the time and weather in a city."
+    ),
+    instruction=(
+        "You are a helpful agent who can answer user questions about the time and weather in a city."
+    ),
+    tools=[get_weather, get_current_time],
+)
